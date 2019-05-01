19a0584be5da96b697d4410e7364f607785d5daf
AsrBassam <45674276+AsrBassam@users.noreply.github.com>
2019-05-01 23:25:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("Family",{type: 'WATCHING'})
+   client.user.setActivity("Family | 1play",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
